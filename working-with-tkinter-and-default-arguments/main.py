781597954f20eb5f8d14364256fe0c838c330c6d
fiona <fiona.kitchen@yahoo.co.uk>
2023-02-09 15:10:43 +0000

    miles to km converter, using grid layouts

--- working-with-tkinter-and-default-arguments/main.py
+++ working-with-tkinter-and-default-arguments/main.py
@@ -1,25 +1,40 @@
 import tkinter
 
 window= tkinter.Tk()
-window.title("My first GUI program")
-window.minsize(width=500, height=300)
+window.title("Miles to Km Converter")
+window.minsize(width=400, height=100)
 
 # first we have to create a component and then put it somehwere
-my_label = tkinter.Label(text='I am a label', font=("Arial", 24, "bold"))
-# pack puts it on the screen 
-my_label.pack()
-
-input = tkinter.Entry()
-input.pack()
-answer=input.get()
+# # pack puts it on the screen - takes params on layout
+# .place()  will give u more finseese
+# grid is beautiful 
 
 def button_clicked():
-    print("i got clicked")
-    answer=input.get()
-    my_label.config(text=answer)
+    
+    answer=float(input.get()) * 1.609
+    conversion.config(text=answer)
 
-button=tkinter.Button(text="Click me", command=button_clicked)
-button.pack()
+
+input = tkinter.Entry()
+input.grid(column=1,row=0)
+answer=input.get()
+
+conversion = tkinter.Label(text="0", font=("Arial", 24, "bold"))
+conversion.grid(column=1,row=1)
+
+equal_to = tkinter.Label(text="is equal to", font=("Arial", 24, "bold"))
+equal_to.grid(column=0, row=1)
+
+miles=tkinter.Label(text="miles", font=("Arial", 24, "bold"))
+miles.grid(column=2,row=0)
+
+km=tkinter.Label(text="Km", font=("Arial", 24, "bold"))
+km.grid(column=2,row=1)
+
+button=tkinter.Button(text="Calculate", command=button_clicked)
+button.grid(column=1,row=2)
+
+
 
 #entry component
 
