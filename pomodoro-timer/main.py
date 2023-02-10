1883b90f75090a86f7570f46fa8eb78c70091a35
fiona <fiona.kitchen@yahoo.co.uk>
2023-02-10 12:47:20 +0000

    my check is misbehaving but everthing else works so

--- pomodoro-timer/main.py
+++ pomodoro-timer/main.py
@@ -7,23 +7,50 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps=0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def on_start():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_secs= WORK_MIN * 60
+    short_break = SHORT_BREAK_MIN*60
+    long_break= LONG_BREAK_MIN*60
+    if reps % 8 ==0:
+        count_down(long_break)
+        title.config(text="Break", fg=RED)
+    elif reps % 2 ==0:
+        count_down(short_break)
+        title.config(text="Break", fg=PINK)
+        
+    else:
+        count_down(work_secs)
+        title.config(text="Work", fg=RED)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     count_min = math.floor(count/60)
-    count_sec = count % 60 
+    count_sec = count % 60
+    if count_sec ==0:
+        count_sec="00"
+    elif count_sec < 10:
+        count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count >0:
        window.after(1000, count_down, count-1)
+    else:
+        on_start()
+        mark = ""
+        for _ in range(math.floor(reps/2)):
+            mark +="✔",
+            checkmark.config(text=mark)
+
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -49,7 +76,7 @@ start= Button(text="Start", font=(FONT_NAME, 12, "normal"), highlightthickness=0
 start.grid(row=2, column=0)
 reset= Button(text="Reset", font=(FONT_NAME, 12, "normal"),highlightthickness=0)
 reset.grid(row=2, column=2)
-checkmark= Label(text="✔", font=(FONT_NAME, 20, "normal"), fg=GREEN)
+checkmark= Label(text="", font=(FONT_NAME, 20, "normal"), fg=GREEN)
 checkmark.grid(row=2, column=1)
 
 
