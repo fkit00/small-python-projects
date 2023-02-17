e75c112b8d91a054ce0d8a31d8e944128ff9bdeb
fiona <fiona.kitchen@yahoo.co.uk>
2023-02-17 10:14:46 +0000

    implemented try catch

--- nato-alphabet/main.py
+++ nato-alphabet/main.py
@@ -7,8 +7,14 @@ data =pandas.read_csv("nato_phonetic_alphabet.csv")
 # we want to use iterrows 
 phonetic_dictionary={row.letter:row.code for (index,row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-# now we need to check against our dictionaru 
-print([phonetic_dictionary[item] for item in word])
+def generate_phonetic (): 
+    word = input("Enter a word: ").upper()
+    try:
+        output_list=[phonetic_dictionary[item] for item in word]
+    except KeyError:
+        print("Sorry only letters in the alphabet please")
+        generate_phonetic() 
+    else:
+        print(output_list)
 
+generate_phonetic()
