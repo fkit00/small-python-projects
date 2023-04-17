2fc0fc90dda096c149ed10a20cf7fb400a1b09c1
fiona <fiona.kitchen@yahoo.co.uk>
2023-04-17 10:48:42 +0100

    made the ui

--- flash-card-app/main.py
+++ flash-card-app/main.py
@@ -1,2 +1,29 @@
+
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title("Flashy")
+window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+card_front_img=PhotoImage(file="python\\small-python-projects\\flash-card-app\\images\\card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="title", font=("Ariel",48,"italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel",60,"bold"))
+
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+cross_image=PhotoImage(file="python\\small-python-projects\\flash-card-app\\images\\wrong.png")
+unknown_button=Button(image=cross_image,  highlightthickness=0)
+unknown_button.grid(row=1, column=0)
+
+check_image=PhotoImage(file="python\\small-python-projects\\flash-card-app\\images\\right.png")
+check_button=Button(image=check_image,  highlightthickness=0)
+check_button.grid(row=1, column=1)
+
+
+
+window.mainloop()
