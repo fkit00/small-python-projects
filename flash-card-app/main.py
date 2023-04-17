b10d100f273575c508cf73da07cf9412fcecfcb0
fiona <fiona.kitchen@yahoo.co.uk>
2023-04-17 14:50:08 +0100

    populated flash cards

--- flash-card-app/main.py
+++ flash-card-app/main.py
@@ -1,7 +1,24 @@
 
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data =pandas.read_csv("python\\small-python-projects\\flash-card-app\\data\\french_words.csv")
+
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card=random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+    
+
+
+
+
 
 window = Tk()
 window.title("Flashy")
@@ -10,20 +27,20 @@ window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img=PhotoImage(file="python\\small-python-projects\\flash-card-app\\images\\card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="title", font=("Ariel",48,"italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel",60,"bold"))
+card_title=canvas.create_text(400, 150, text="title", font=("Ariel",48,"italic"))
+card_word=canvas.create_text(400, 263, text="word", font=("Ariel",60,"bold"))
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image=PhotoImage(file="python\\small-python-projects\\flash-card-app\\images\\wrong.png")
-unknown_button=Button(image=cross_image,  highlightthickness=0)
+unknown_button=Button(image=cross_image,  highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image=PhotoImage(file="python\\small-python-projects\\flash-card-app\\images\\right.png")
-check_button=Button(image=check_image,  highlightthickness=0)
+check_button=Button(image=check_image,  highlightthickness=0, command=next_card)
 check_button.grid(row=1, column=1)
 
-
+next_card()
 
 window.mainloop()
