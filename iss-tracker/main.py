cc6d9495323b6b377763c6a8b0a794eae7fd9d87
fiona <fiona.kitchen@yahoo.co.uk>
2023-04-19 13:41:14 +0100

    playing around with api params in python using sunset

--- iss-tracker/main.py
+++ iss-tracker/main.py
@@ -1,7 +1,26 @@
 import requests
 
-response =requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+MY_LAT = 51.507351
+MY_LONG =  -0.127758
 
-data = response.json()["iss_position"]
+# response =requests.get(url="http://api.open-notify.org/iss-now.json")
+# print(response)
+
+# data = response.json()["iss_position"]
+# print(data)
+
+# longitude = response.json()["iss_position"]["longitude"]
+# latitude = response.json()["iss_position"]["latitude"]
+
+# iss_positon=(longitude, latitude)
+# print(iss_positon)
+
+
+params ={
+    "lat":MY_LAT,
+    "lng":MY_LONG
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=params)
+data=response.json()
 print(data)
