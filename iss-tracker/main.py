24f715f347ce85b22c6110341be9165b1d13bd35
fiona <fiona.kitchen@yahoo.co.uk>
2023-04-19 14:01:12 +0100

    working on processing json data

--- iss-tracker/main.py
+++ iss-tracker/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 MY_LAT = 51.507351
 MY_LONG =  -0.127758
@@ -18,9 +19,18 @@ MY_LONG =  -0.127758
 
 params ={
     "lat":MY_LAT,
-    "lng":MY_LONG
+    "lng":MY_LONG,
+    "formatted":0
 }
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=params)
 data=response.json()
 print(data)
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+time_now=datetime.now()
+print(time_now)
+print(sunrise.split("T")[1].split(":")[0],sunset.split("T")[1].split(":")[0])
+print(time_now.hour)
+# this gives us the hours of the sunrise times
