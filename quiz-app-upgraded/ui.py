b5df464b63485e294fc54699915d991fddb50e52
fiona <fiona.kitchen@yahoo.co.uk>
2023-04-24 10:12:09 +0100

    ui pretty much completed

--- quiz-app-upgraded/ui.py
+++ quiz-app-upgraded/ui.py
@@ -6,7 +6,25 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.config(padx=20,pady=20, bg=THEME_COLOR)
+
+        self.score_label=Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas= Canvas(width=300, height=250, bg="white")
+        self.question_text= self.canvas.create_text(150, 125, 
+                                                    text="some question text", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image=PhotoImage(file="python\\small-python-projects\\quiz-app-upgraded\\images\\false.png")
+        false_image=PhotoImage(file="python\\small-python-projects\\quiz-app-upgraded\\images\\true.png")
+        self.true_button=Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+        self.false_button=Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+   
         
-        
+
+
         self.window.mainloop()
 
