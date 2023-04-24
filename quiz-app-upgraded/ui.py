008e6da62ba023fec6edf3435381b4adb4456fdd
fiona <fiona.kitchen@yahoo.co.uk>
2023-04-24 11:35:07 +0100

    nealy finished ui and learnt more about type hints

--- quiz-app-upgraded/ui.py
+++ quiz-app-upgraded/ui.py
@@ -1,9 +1,11 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain:QuizBrain):
+        self.quiz=quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20,pady=20, bg=THEME_COLOR)
@@ -12,7 +14,7 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas= Canvas(width=300, height=250, bg="white")
-        self.question_text= self.canvas.create_text(150, 125, 
+        self.question_text= self.canvas.create_text(150, 125,width=280, 
                                                     text="some question text", fill=THEME_COLOR, font=("Arial", 20, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
@@ -23,8 +25,11 @@ class QuizInterface:
         self.false_button=Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
    
-        
+        self.get_next_question()
 
 
         self.window.mainloop()
-
+    
+    def get_next_question(self):
+        q_text= self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
